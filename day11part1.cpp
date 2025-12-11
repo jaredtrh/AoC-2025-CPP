d128b6827a9ce5ef50aeb8a654e1c816dc6efa27
jared <jared@yellresearch.com>
2025-12-11 13:34:23 +0800

    day11

---
+++ day11part1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split(const string& s, const string& delim) {
+    vector<string> res;
+    int pos = 0;
+    while (true) {
+        int nxt = s.find(delim, pos);
+        if (nxt == string::npos) {
+            res.push_back(s.substr(pos));
+            break;
+        }
+        res.push_back(s.substr(pos, nxt - pos));
+        pos = nxt + delim.size();
+    }
+    return res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    freopen("input.txt", "r", stdin);
+
+    map<string, vector<string>> adj;
+
+    string line;
+    while (getline(cin, line)) {
+        auto s = split(line, ": ");
+        adj[s[0]] = split(s[1], " ");
+    }
+
+    map<string, int> dp = {{"out", 1}};
+    auto dfs = [&](const auto& self, const string& s) -> int {
+        if (dp.count(s)) return dp[s];
+        int res = 0;
+        for (const auto& nxt : adj[s]) {
+            res += self(self, nxt);
+        }
+        return dp[s] = res;
+    };
+
+    cout << dfs(dfs, "you") << "\n";
+}
